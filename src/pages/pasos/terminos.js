efacc7cc9cd8cab3db6953817316f4630b84be3c
Miguel Angel Reyes Moreno <miguelreyesmoreno@hotmail.com>
2023-03-31 13:01:13 -0600

    modal logic

--- src/pages/pasos/terminos.js
+++ src/pages/pasos/terminos.js
@@ -1,19 +1,37 @@
-import { useState } from "react"
+import { useState, useEffect, useRef } from "react"
 import { GoBack } from "@/components/GoBack"
 import { ProgressBar } from "@/components/ProgressBar"
 import { OrangeButton } from "@/components/orangeButton"
-import Link from "next/link"
 
 export default function Terminos() {
   const [checked, setChecked] = useState(false)
+  const [showModal, setShowModal] = useState(false)
+  const modalRef = useRef(null)
 
   const handleCheck = () => {
     setChecked(!checked)
   }
 
+  const handleModal = () => {
+    setShowModal(true)
+  }
+
   const nextPage = () => {
     window.location.href = '/pasos/final/'
   }
+
+  const handleClickOutside = (event) => {
+    if (modalRef.current && !modalRef.current.contains(event.target)) {
+      setShowModal(false)
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside)
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [])
 
   return (
     <>
@@ -22,18 +40,29 @@ export default function Terminos() {
       <GoBack link='/pasos/verificacion/'></GoBack>
       <h1>Términos y condiciones</h1>
       <p>Por favor revisa nuestros términos y condiciones para este servicio:</p>
-      <Link href="#">
+      <button onClick={handleModal}>
         Consulta términos y condiciones
-      </Link>
+      </button>
       <label htmlFor="terminos">
         <input type="checkbox" id="terminos" value={checked} onChange={handleCheck} />
         Acepto los términos y condiciones
       </label>
+      <br />
       <OrangeButton
         text='Enviar'
         disabled={!checked}
         func={nextPage}
       />
+      {
+        showModal && (
+          <section ref={modalRef} style={{border: '1px solid black'}}>
+            <h3>Términos y condiciones</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore nesciunt ipsam impedit laudantium dignissimos in illo hic eos, quisquam consequatur laborum molestias voluptatibus ex magni, voluptatem ad maxime cupiditate beatae.
+            </p>
+          </section>
+        )
+      }
     </>
   )
 }
